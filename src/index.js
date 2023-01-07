948597415dd01c0c0b6a5871a639fbbce37c2154
Andrej1405 <andrej.udentsov@yandex.ru>
2023-01-07 21:49:10 +0500

    Replace ball, minor change in code

--- src/index.js
+++ src/index.js
@@ -2,24 +2,24 @@ import {createCanvas, drawStar} from "./helpers"
 
 const NUMBER_STARS_CANVAS = 100
 const PLATFORM_WIDTH = 200
-const PLATFORM_HEIGHT = 30
-const BALL_WIDTH = 25
-const BALL_HEIGHT = 25
-const KEYBORD_KEYS = [['ArrowLeft', 'a'], ['ArrowRight', 'd'], ['Space']]
+const PLATFORM_HEIGHT = 24
+const BALL_RADIUS = 18
+const KEYBORD_KEYS = [['ArrowLeft', 'KeyA'], ['ArrowRight', 'KeyD'], ['Space']]
 
 const platform = {
 	x: PLATFORM_WIDTH,
 	y: PLATFORM_HEIGHT,
-	cx: window.innerWidth / 2 - PLATFORM_WIDTH / 2,
-	cy: window.innerHeight - PLATFORM_HEIGHT * 2,
+	cx: innerWidth / 2 - PLATFORM_WIDTH / 2,
+	cy: innerHeight - PLATFORM_HEIGHT * 2,
+	color: '#A69200',
 }
 
 const ball = {
 	isMovement: false,
-	x: BALL_WIDTH,
-	y: BALL_HEIGHT,
-	cx: platform.cx + platform.x / 2 - BALL_WIDTH / 2,
-	cy: platform.cy - BALL_HEIGHT - 2,
+	radius: BALL_RADIUS,
+	cx: platform.cx + platform.x / 2 - BALL_RADIUS / 2 + BALL_RADIUS / 2,
+	cy: platform.cy - BALL_RADIUS - 3,
+	color: '#A65800',
 }
 
 ;(function main() {
@@ -31,14 +31,14 @@ const ball = {
 
 	// Render backCanvas with background and stars
 	const render = () => {
-		backCanvasCtx.fillStyle = '#071672'
+		backCanvasCtx.fillStyle = '#020C4A'
     backCanvasCtx.fillRect(0, 0, backCanvas.width, backCanvas.height)
 
 		for (let i = 0; i < NUMBER_STARS_CANVAS; i++) {
 			const cx = 8
 			const cy = 3
-			const x = Math.random() * backCanvas.width
-			const y = Math.random() * backCanvas.height
+			const x = Math.random() * (backCanvas.width - cx) + cx
+			const y = Math.random() * (backCanvas.height - cy) + cy
 		
 			drawStar(backCanvasCtx, x, y, cx, cy)
 		}
@@ -51,28 +51,34 @@ const ball = {
 
 		frontCanvasCtx.clearRect(0, 0, frontCanvas.width, frontCanvas.height)
 
+		frontCanvasCtx.fillStyle = platform.color
 		frontCanvasCtx.fillRect(platform.cx, platform.cy, platform.x, platform.y)
-		frontCanvasCtx.fillRect(ball.cx, ball.cy, ball.x, ball.y)
-
+		
+		frontCanvasCtx.fillStyle = ball.color
+		frontCanvasCtx.beginPath()
+		frontCanvasCtx.arc(ball.cx, ball.cy, ball.radius, 0, 2 * Math.PI)
+		frontCanvasCtx.closePath()
+		frontCanvasCtx.fill()
+		
 		if (ball.isMovement) {
 			ball.cy -= 2;
 		}
 	}
 	
 	window.addEventListener('resize', () => {
-		backCanvas.setAttribute('width', window.innerWidth)
-		backCanvas.setAttribute('height', window.innerHeight)
+		backCanvas.setAttribute('width', innerWidth)
+		backCanvas.setAttribute('height', innerHeight)
 
-		frontCanvas.setAttribute('width', window.innerWidth)
-		frontCanvas.setAttribute('height', window.innerHeight)
+		frontCanvas.setAttribute('width', innerWidth)
+		frontCanvas.setAttribute('height', innerHeight)
 
 		requestAnimationFrame(render)
 	})
 
 	window.addEventListener('keydown', e => {
-		if (!KEYBORD_KEYS.flat(1).includes(e.key) && !KEYBORD_KEYS.flat(1).includes(e.code)) return
-		if (KEYBORD_KEYS[0].includes(e.key)) platform.cx -= 6
-		if (KEYBORD_KEYS[1].includes(e.key)) platform.cx += 6
+		if (!KEYBORD_KEYS.flat(1).includes(e.code)) return
+		if (KEYBORD_KEYS[0].includes(e.code)) platform.cx -= 6
+		if (KEYBORD_KEYS[1].includes(e.code)) platform.cx += 6
 		if (KEYBORD_KEYS[2].includes(e.code)) ball.isMovement = !ball.isMovement
 	})
 
