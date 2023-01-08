1a8d92a379c5d55b5e77955860b16faa9a813415
Andrej1405 <andrej.udentsov@yandex.ru>
2023-01-08 15:13:38 +0500

    Add server for stasrt game

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+const webpackMiddleware = require("webpack-dev-middleware")
+const config = require('./webpack.config.js')
+const webpack = require('webpack')
+const compiler = webpack(config)
+
+const PORT = process.env.PORT || 8080
+
+const app = express()
+app.use(webpackMiddleware(compiler, {
+  publicPath: '/dist/',
+}))
+
+app.get('/', (_, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'))
+})
+
+app.listen(PORT, () => {
+	console.info(`Server started on port: ${PORT}`)
+})
