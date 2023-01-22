1ef728ceeedf48532a752b5ffe7a9ac3a5bac234
Andrej1405 <andrej.udentsov@yandex.ru>
2023-01-22 20:33:02 +0500

    Chahge command, format code

--- README.md
+++ README.md
@@ -4,11 +4,13 @@ The basic implementation of the `Arkanoid` game.\
 All shapes are drawn using standard canvas methods.
 
 Run the command to build the project:
+
 ```
 npm run build
 ```
 
 Run to start server:
+
 ```
 npm run start
 ```
--- server.js
+++ server.js
@@ -8,14 +8,16 @@ const compiler = webpack(config)
 const PORT = process.env.PORT || 8080
 
 const app = express()
-app.use(webpackMiddleware(compiler, {
-  publicPath: '/dist/',
-}))
+app.use(
+  webpackMiddleware(compiler, {
+    publicPath: '/dist/',
+  })
+)
 
 app.get('/', (_, res) => {
   res.sendFile(path.resolve(__dirname, 'public/index.html'))
 })
 
 app.listen(PORT, () => {
-	console.info(`Server started on port: ${PORT}`)
+  console.info(`Server started on port: ${PORT}`)
 })
